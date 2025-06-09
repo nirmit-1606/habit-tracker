98389a748994ce7cf7d2711c4a57c239a3c7cdc6
Nirmit P <nirmitpatel16620@gmail.com>
2025-06-09 17:33:14 -0500

    use env for firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -9,13 +9,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDsv_-kc7uS9DE6YZQUoagdISswOdF-LaY",
-  authDomain: "habit-tracker-dd506.firebaseapp.com",
-  projectId: "habit-tracker-dd506",
-  storageBucket: "habit-tracker-dd506.firebasestorage.app",
-  messagingSenderId: "1007983228080",
-  appId: "1:1007983228080:web:b22911505a0fd9fd077775",
-  measurementId: "G-KZ039SFEVG"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
